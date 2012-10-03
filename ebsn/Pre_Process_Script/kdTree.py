9148b3bb7f67cfb8c29bda726f8a088b89aabc19
zhangwei <zhangwei@zhangwei-laptop.(none)>
2012-10-03 21:10:40 +0800

    modified:   Pre_Process_Script/kdTree.py

--- ebsn/Pre_Process_Script/kdTree.py
+++ ebsn/Pre_Process_Script/kdTree.py
@@ -15,7 +15,8 @@ from geopy import distance
 dimNum = 2
 # Finish Points
 totalFinishCnt = 0
-#
+# Distance threshold
+mileThreshold = 20
 
 # Create KD-tree
 # ================
@@ -75,7 +76,6 @@ def iterCreateTree(root, treedep, leafset):
     raw_input()'''
     iterCreateTree(root[median_num][1], treedep, right_son_tree)
 
-
 def median(numbers):
     '''Return the median of the list of numbers.'''
     # Sort the list of numbers and take the middle element.
@@ -89,20 +89,75 @@ def median(numbers):
 
 # Search KD-tree
 # ==============
-def searchKDtree(searchDataFile, outputFile):
+def searchKDtree(kdTree, searchDataFile, outputFile):
+    wfd = open(outputFile, 'w')
     for line in open(searchDataFile):
+        res = line.split(',')
+        user_id = res[0]
+        location_val = map(lambda x: float(x), res[1:])
+
+        # Find the nearest negibhor and get search path
+        depth = 0
+        search_path = []
+        near_events = []
+        searchNearestNeighbor(location_val, kdTree, search_path, depth, near_events)
+
+        # Get the list of events which meets the distance requirement
+        backSearchTree(location_val, search_path, near_events)
+        print user_id
+        print near_events
+        raw_input()
+        for event_id in near_events:
+            wfd.write("%s,%s\n" % (user_id, event_id))
+    wfd.close()
+
+def searchNearestNeighbor(locationVal, kdTree, searchPath, depth, near_events):
+    dim = depth%dimNum
+    key = kdTree.keys()
+    # Terminal Condition --> reach the leaf
+    if type(kdTree[key][0]) == 'int' or type(kdTree[key][0]) == 'float':
+        for key in kdTree.keys():
+            if distance.distance(locationVal, kdTree[key]).miles <= mileThreshold:
+                near_events.append(key)
+        return
+
+    # Tree's interior node should meet the tree structure
+    if len(key) != 1:
+        print 'Not tree structure.'
+        sys.exit(1)
+
+    key = key[0]
+    # Iterative Search
+    depth += 1
+    if locationVal[dim] < key:
+        searchPath.append(kdTree[key][1])  # Note: store the reverse search direction of point.
+        searchNearestNeighbor(locatioinVal, kdTree[key][0], searchPath, depth)
+    else:
+        searchPath.append(kdTree[key][0])
+        searchNearestNeighbor(locatioinVal, kdTree[key][1], searchPath, depth)
+
+def backSearchTree(locationVal, searchPath, near_events):
+    for root in reversed(searchPath):
+        if traverseTree(root, locationVal, near_events):
+            return
+
+def traverseTree(root, locationVal, near_events):
+    '''
+    Binary tree traversal
+    '''
 
 
-
+# Main function
+# =============
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print 'usage: <event_location.in> <user_location.in> <user_event_near.out>'
         sys.exit(1)
 
     print "Start creating KD tree."
-    kdTree = createKDtree(sys.argv[1])
+    kd_tree = createKDtree(sys.argv[1])
     print "Finish creating KD tree."
-    #searchKDtree(kdTree, sys.argv[2], sys.argv[3])
+    searchKDtree(kdTree, sys.argv[2], sys.argv[3])
 
     '''# Test
     testEvent = {'1':[1,1], '2':[1,2], '3':[2,1], '4':[2,2]}
