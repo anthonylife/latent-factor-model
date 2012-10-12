c5a3e22aa41adfebbe75ed84c91776cc5d822c34
zhangwei <zhangwei@zhangwei-laptop.(none)>
2012-10-12 14:40:09 +0800

    modified:   Feature_Generation_Script/rSample.py 	modified:   mkFeatures.sh

--- ebsn/Feature_Generation_Script/rSample.py
+++ ebsn/Feature_Generation_Script/rSample.py
@@ -2,18 +2,51 @@
 #encoding=utf8
 
 # This script can the input file by line into specified number of parts.
+# Note: Sampling process of train and test set partition should satisfy the following requirement:
+#       ---- Every user and group should occur at least one time in training data set.
+#   In order to obey the above rule, I adopt the following method:
+#       1.Create two dictionaries, one using "user" as key and the other using "group" as key.
+#       2.Loops over user keys and for each key, sample one group as training data. After finishing
+#           this process, we can ensure every user id will occur in train data.
+#       3.Check whether every group id occured in train data. For group id not occuring in train
+#           data, sample one user as training data. After doing this, we can assure every group
+#           id will occur in train data.
+#       4.For the left data, randomly partition them into train and test data accroding to the
+#           specified ratio.
+
+'''
+python Feature_Generation_Script/rSample.py Clean_Data/NewYork_user_group.ridx.csv 0.8 Train_Test_Data/NewYork_user_group.train.csv Train_Test_Data/NewYork_user_group.test.csv
+'''
 
 import sys
 import random
+from math import ceil
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print 'usage: <file.in> <samples_num.in> <file.out>'
+    if len(sys.argv) != 5:
+        print 'usage: <file.in> <samples_num.in> <train_file.out> <test_file.out>'
         sys.exit(1)
 
-    item_set = []
+    tra_ins = []
+    tes_ins = []
+    gkey_set = set([])
+    tra_idx = 0
+    all_cnt = 0
+
+    # 1.Create two dictionaries, one using "user" as key and the other using "group" as key.
+    ukey_item = {}
+    gkey_item = {}
     for line in open(sys.argv[1]):
-        item_set.append(line)
+        res = line.strip('\n').split(',')
+        all_cnt += 1
+        if res[0] in ukey_item:
+            ukey_item[res[0]].append(res[1])
+        else:
+            ukey_item[res[0]] = res[1:]
+        if res[1] in gkey_item:
+            gkey_item[res[1]].append(res[0])
+        else:
+            gkey_item[res[1]] = res[:1]
 
     # Sample accroding to the specified number
     if sys.argv[2].find('.') == -1:
@@ -21,11 +54,47 @@ if __name__ == "__main__":
     # Sample accroding to the specified ratio
     else:
         ratio = float(sys.argv[2])
-        sample_cnt = len(item_set)*ratio
+        sample_cnt = int(ceil(all_cnt*ratio))
 
+    # 2.Loops over user keys and for each key, sample one group as training data. After finishing
+    # this process, we can ensure every user id will occur in train data.
+    for uid in ukey_item:
+        gid = random.sample(ukey_item[uid], 1)
+        ukey_item[uid].remove(gid[0])
+        tra_ins.append([uid, gid[0]])
+        gkey_set.add(gid[0])
+        tra_idx += 1
+
+    # 3.Check whether every group id occured in train data. For group id not occuring in train
+    # data, sample one user as training data. After doing this, we can assure every group
+    # id will occur in train data.
+    for gid in gkey_item:
+        if gid not in gkey_set:
+            gkey_set.add(gid)
+            uid = random.sample(gkey_item[gid], 1)
+            ukey_item[uid[0]].remove(gid)
+            tra_ins.append([uid[0], gid])
+            tra_idx += 1
+
+    # 4.For the left data, randomly partition them into train and test data accroding to the
+    # specified ratio.
+    left_dset = []
+    for uid in ukey_item:
+        for gid in ukey_item[uid]:
+            left_dset.append([uid, gid])
+    sample_ins = random.sample(left_dset, sample_cnt - tra_idx)
+    tra_ins = tra_ins + sample_ins
+    left_dset = map(lambda x: str(x[0])+'_'+str(x[1]), left_dset)
+    sample_ins = map(lambda x: str(x[0])+'_'+str(x[1]), sample_ins)
+    tes_ins = set(left_dset) - set(sample_ins)
+    tes_ins = map(lambda x: [x.split('_')[0], x.split('_')[1]], tes_ins)
+
+    # Output train and test segmentation result
     wfd = open(sys.argv[3], 'w')
-    samples = set(random.sample(item_set, sample_cnt))
-    item_set = item_set-samples
-    for sample in samples:
-        wfd.write(sample)
+    for tra in tra_ins:
+        wfd.write("%s,%s\n" % (tra[0], tra[1]))
+    wfd.close()
+    wfd = open(sys.argv[4], 'w')
+    for tes in tes_ins:
+        wfd.write("%s,%s\n" % (tes[0], tes[1]))
     wfd.close()
--- ebsn/mkFeatures.sh
+++ ebsn/mkFeatures.sh
@@ -2,7 +2,8 @@
 
 ##This script makes use of many other scripts in "Feature_Generation_Script" to make feature data for training and testing.
 
-# Main procedure:
+# Main procedures:
+#   0. Reindex all data
 #   1. Partition raw data
 #   2. Sample negative instances of each positive instance
 #   3. Combine positive instance and negative instance 
@@ -31,30 +32,70 @@ PartitionNum=5
 Ratio=0.8
 # ratio of the number of negative and positive instances in training data
 RatioPN=4
+# ID of the leaf segment data
+LeftID=5
 
+
+## Runing procedures
+# 1.Reindex all data
+#echo 'Reindex all data...'
+#python $ScriptRootPath/reIndex.py $DataRootPath/NewYork_user.csv $DataRootPath/NewYork_group.csv $DataRootPath/NewYork_tag.csv $DataRootPath/NewYork_event.csv $DataRootPath/NewYork_event_group.csv $DataRootPath/NewYork_group_tag.csv $DataRootPath/NewYork_tag_text.csv $DataRootPath/NewYork_user_event.csv $DataRootPath/NewYork_user_group.csv $DataRootPath/NewYork_user_lon_lat.csv $DataRootPath/NewYork_user_tag.csv $DataRootPath/NewYork_event_lon_lat.csv $DataRootPath/NewYork_user.ridx.csv $DataRootPath/NewYork_group.ridx.csv $DataRootPath/NewYork_tag.ridx.csv $DataRootPath/NewYork_event.ridx.csv $DataRootPath/NewYork_event_group.ridx.csv $DataRootPath/NewYork_group_tag.ridx.csv $DataRootPath/NewYork_tag_text.ridx.csv $DataRootPath/NewYork_user_event.ridx.csv $DataRootPath/NewYork_user_group.ridx.csv $DataRootPath/NewYork_user_lon_lat.ridx.csv $DataRootPath/NewYork_user_tag.ridx.csv $DataRootPath/NewYork_event_lon_lat.ridx.csv
 
 # 1. Partition raw data
-#python $ScriptRootPath/rPartition.py $DataRootPath/NewYork_user_group.csv $PartitionNum $TrainDataRootPath/NewYork_user_group.pos
+#echo 'Partition raw data...'
+#python $ScriptRootPath/rPartition.py $DataRootPath/NewYork_user_group.ridx.csv $PartitionNum $TrainDataRootPath/NewYork_user_group.pos
 
-# 2. Sample negative instances of each positive instance
-#python $ScriptRootPath/getNegInstance.py $DataRootPath/NewYork_user_group.csv $DataRootPath/NewYork_group.csv $TrainDataRootPath  $RatioPN NewYork_user_group.pos
+# 2. Combine positive train instances
+#echo 'Combine positive instance...'
+#python $ScriptRootPath/mergePoNeIns.py $TrainDataRootPath 'pos' $LeftID $TrainDataRootPath/NewYork_user_group.train.csv $TrainDataRootPath/NewYork_rate_user_group.train.csv
+#cp $TrainDataRootPath/NewYork_user_group.train.csv $FeatureRootPath/NewYork_user_group.train.csv
+#cp $TrainDataRootPath/NewYork_user_group.pos.$LeftID $TrainDataRootPath/NewYork_user_group.test.csv
 
-# 3. Combine positive instance and negative instance 
-#python $ScriptRootPath/mergePoNeIns.py $TrainDataRootPath 'pos,neg' 5 $TrainDataRootPath/NewYork_user_group.train.csv $TrainDataRootPath/NewYork_rate_user_group.train.csv
+# 2. Randomly partition train data and test data
+echo 'Randomly partition train data and test data accroding to specified ratio...'
+python $ScriptRootPath/rSample.py $DataRootPath/NewYork_user_group.ridx.csv $Ratio $TrainDataRootPath/NewYork_user_group.train.csv $TrainDataRootPath/ NewYork_user_group.test.csv
+
+# 3. Sample negative instances of each positive instance and segment them into train and test data
+echo 'Sample negative instances of each positive instance...'
+python $ScriptRootPath/getNegInstance.py $DataRootPath/NewYork_user_group.ridx.csv $DataRootPath/NewYork_group.ridx.csv $TrainDataRootPath/NewYork_user_group.train.csv $RatioPN $TrainDataRootPath/NewYork_rate_user_group.train.csv
+python $ScriptRootPath/getNegInstance.py $DataRootPath/NewYork_user_group.ridx.csv $DataRootPath/NewYork_group.ridx.csv $TrainDataRootPath/NewYork_user_group.test.csv $RatioPN $TrainDataRootPath/NewYork_rate_user_group.test.csv
+cp $TrainDataRootPath/NewYork_rate_user_group.train.csv $FeatureRootPath/NewYork_rate_user_group.train.csv
+cp $TrainDataRootPath/NewYork_rate_user_group.test.csv $FeatureRootPath/NewYork_rate_user_group.test.csv
 
 # 4. Make different features for each train instance
 #   4.1 make group activeness-based features
-#python $ScriptRootPath/mkGroupActiveFeatures.py $TrainDataRootPath/NewYork_user_group.train.csv $DataRootPath/NewYork_event_group.csv $DataRootPath/NewYork_event_lon_lat.csv $TrainDataRootPath/NewYork_rate_user_group.train.csv $FeatureRootPath/NewYork_group_activeness.features.csv
+echo 'make group activeness-based features...'
+python $ScriptRootPath/mkGroupActiveFeatures.py $TrainDataRootPath/NewYork_user_group.train.csv $DataRootPath/NewYork_event_group.ridx.csv $DataRootPath/NewYork_event_lon_lat.ridx.csv $TrainDataRootPath/NewYork_rate_user_group.train.csv $FeatureRootPath/NewYork_group_activeness.train.features.csv
+python $ScriptRootPath/mkGroupActiveFeatures.py $TrainDataRootPath/NewYork_user_group.train.csv $DataRootPath/NewYork_event_group.ridx.csv $DataRootPath/NewYork_event_lon_lat.ridx.csv $TrainDataRootPath/NewYork_rate_user_group.test.csv $FeatureRootPath/NewYork_group_activeness.test.features.csv
 
 #   4.2 make user group relation features
-#python $ScriptRootPath/mkUGRelation.py $TrainDataRootPath/NewYork_user_group.train.csv $TrainDataRootPath/NewYork_rate_user_group.train.csv $FeatureRootPath/NewYork_user_group_sim.features.csv
+echo 'make user group relation features...'
+python $ScriptRootPath/mkUGRelation.py $TrainDataRootPath/NewYork_user_group.train.csv $TrainDataRootPath/NewYork_rate_user_group.train.csv $FeatureRootPath/NewYork_user_group_sim.train.features.csv
+python $ScriptRootPath/mkUGRelation.py $TrainDataRootPath/NewYork_user_group.train.csv $TrainDataRootPath/NewYork_rate_user_group.test.csv $FeatureRootPath/NewYork_user_group_sim.test.features.csv
 
 #   4.3 make distance-based features
 #       4.3.1 distance between user's home and target group's event
-#python $ScriptRootPath/mkHomeEventDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_lon_lat.csv $DataRootPath/NewYork_event_group.csv $DataRootPath/NewYork_event_lon_lat.csv $FeatureRootPath/NewYork_user_group_event_dis.features.csv
+echo 'distance between user's home and target group's event...'
+python $ScriptRootPath/mkHomeEventDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_lon_lat.ridx.csv $DataRootPath/NewYork_event_group.ridx.csv $DataRootPath/NewYork_event_lon_lat.ridx.csv $FeatureRootPath/NewYork_user_group_event_dis.train.features.csv
+python $ScriptRootPath/mkHomeEventDis.py $TrainDataRootPath/NewYork_rate_user_group.test.csv $DataRootPath/NewYork_user_lon_lat.ridx.csv $DataRootPath/NewYork_event_group.ridx.csv $DataRootPath/NewYork_event_lon_lat.ridx.csv $FeatureRootPath/NewYork_user_group_event_dis.test.features.csv
+
 #       4.3.2 distance between user's home and home of users in target group
-#python $ScriptRootPath/mkHomeHomeDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_lon_lat.csv $DataRootPath/NewYork_user_group.csv $FeatureRootPath/NewYork_user_group_user_dis.features.csv
+echo "distance between user's home and home of users in target group..."
+python $ScriptRootPath/mkHomeHomeDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_lon_lat.ridx.csv $DataRootPath/NewYork_user_group.ridx.csv $FeatureRootPath/NewYork_user_group_user_dis.train.features.csv
+python $ScriptRootPath/mkHomeHomeDis.py $TrainDataRootPath/NewYork_rate_user_group.test.csv $DataRootPath/NewYork_user_lon_lat.ridx.csv $DataRootPath/NewYork_user_group.ridx.csv $FeatureRootPath/NewYork_user_group_user_dis.test.features.csv
+
 #       4.3.3 distance between the locations of the events user joined and the events in target groups
-python $ScriptRootPath/mkEventEventDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_event.csv $DataRootPath/NewYork_event_lon_lat.csv $DataRootPath/NewYork_event_group.csv $FeatureRootPath/NewYork_event_group_event_dis.features.csv
+#echo "distance between the locations of the events user joined and the events in target groups..."
+#python $ScriptRootPath/mkEventEventDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_event.ridx.csv $DataRootPath/NewYork_event_lon_lat.csv $DataRootPath/NewYork_event_group.ridx.csv $FeatureRootPath/NewYork_event_group_event_dis.train.features.csv
+#python $ScriptRootPath/mkEventEventDis.py $TrainDataRootPath/NewYork_rate_user_group.test.csv $DataRootPath/NewYork_user_event.ridx.csv $DataRootPath/NewYork_event_lon_lat.csv $DataRootPath/NewYork_event_group.ridx.csv $FeatureRootPath/NewYork_event_group_event_dis.test.features.csv
+
 #       4.3.4 distance between the locations of events the user joined and locations of other users in target groups
-python $ScriptRootPath/mkEventHomeDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_event.csv $DataRootPath/NewYork_event_lon_lat.csv $TrainDataRootPath/NewYork_user_group.train.csv $DataRootPath/NewYork_user_lon_lat.csv $DataRootPath/NewYork_event_group.csv $FeatureRootPath/NewYork_event_group_home_dis.features.csv
+#echo "distance between the locations of events the user joined and locations of other users in target groups..."
+#python $ScriptRootPath/mkEventHomeDis.py $TrainDataRootPath/NewYork_rate_user_group.train.csv $DataRootPath/NewYork_user_event.ridx.csv $DataRootPath/NewYork_event_lon_lat.csv $TrainDataRootPath/NewYork_user_group.train.csv $DataRootPath/NewYork_user_lon_lat.ridx.csv $DataRootPath/NewYork_event_group.ridx.csv $FeatureRootPath/NewYork_event_group_home_dis.train.features.csv
+#python $ScriptRootPath/mkEventHomeDis.py $TrainDataRootPath/NewYork_rate_user_group.test.csv $DataRootPath/NewYork_user_event.ridx.csv $DataRootPath/NewYork_event_lon_lat.csv $TrainDataRootPath/NewYork_user_group.train.csv $DataRootPath/NewYork_user_lon_lat.ridx.csv $DataRootPath/NewYork_event_group.ridx.csv $FeatureRootPath/NewYork_event_group_home_dis.test.features.csv
+
+# 5. Combine different feature files into one file
+echo "Combine different feature files into one file..."
+python $ScriptRootPath/combineFeatures.py $FeatureRootPath/NewYork_group_activeness.train.features.csv $FeatureRootPath/NewYork_user_group_sim.train.features.csv $FeatureRootPath/NewYork_user_group_event_dis.train.features.csv $FeatureRootPath/NewYork_user_group_user_dis.train.features.csv $FeatureRootPath/NewYork_event_group_event_dis.train.features.csv $FeatureRootPath/NewYork_event_group_home_dis.train.features.csv $RatioPN $FeatureRootPath/NewYork_train_all.features.csv
+python $ScriptRootPath/combineFeatures.py $FeatureRootPath/NewYork_group_activeness.test.features.csv $FeatureRootPath/NewYork_user_group_sim.test.features.csv $FeatureRootPath/NewYork_user_group_event_dis.test.features.csv $FeatureRootPath/NewYork_user_group_user_dis.test.features.csv $FeatureRootPath/NewYork_event_group_event_dis.test.features.csv $FeatureRootPath/NewYork_event_group_home_dis.test.features.csv $RatioPN $FeatureRootPath/NewYork_test_all.features.csv
+
